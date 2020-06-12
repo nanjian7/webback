4b862e2fb723d7ffcd5c56c28bdac9ffd9608d8f
Administrator <asd@>
2020-06-12 11:10:09 +0800

    为登陆注销做准备

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,22 +1,54 @@
 package cn.wzvtc.soft;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "http://127.0.0.1:5500",allowCredentials = "true")
 @RestController("/")
 public class TestController {
 
     @RequestMapping(value = "/data.json")
-    public List bookById() {
+    public List bookByIdWithAuthened(HttpSession httpSession) {
+        String username = (String) httpSession.getAttribute("username");
+        if (StringUtils.hasText(username)) {
+            return bookById();
+        }else{
+            return null;
+        }
+    }
+
+    @RequestMapping(value = "/login")
+    public boolean login(String username,String password,HttpSession httpSession) {
+        if("2012020045".equals(username) && "lxf".equals(password)){
+            httpSession.setAttribute("useranme","2012020045");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @RequestMapping(value = "/logout")
+    public void logout(HttpSession httpSession) {
+        httpSession.removeAttribute("username");
+    }
+
+
+
+
+
+
+
+    private List bookById() {
+
         List resultList=new ArrayList();
         Map resultMap = new HashMap<>();
         resultMap.put("name", "计算机安装与维护");
@@ -43,7 +75,7 @@ public class TestController {
     }
 
 
-    @RequestMapping(value = "/creditByName", method = {RequestMethod.GET})
+    @RequestMapping(value = "/creditByName")
     public int getCrditByName(String name) {
         if("计算机编程基础".equals(name)){
             return 4;
